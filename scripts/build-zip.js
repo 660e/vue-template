ff26dfdca97a6f477849bee979b39cda7ba1981b
Min <solunar@vip.qq.com>
2026-04-07 11:13:39 +0800

    Update

---
+++ scripts/build-zip.js
@@ -0,0 +1,10 @@
+import fs from 'fs'
+
+import archiver from 'archiver'
+
+const output = fs.createWriteStream('dist.zip')
+const archive = archiver('zip', { zlib: { level: 9 } })
+
+archive.pipe(output)
+archive.directory('dist/', false)
+archive.finalize()
