3cd1b1334d05a805b972497d0a373a67cd16dbfc
Min <solunar@vip.qq.com>
2026-04-27 22:02:43 +0800

    Update

--- src/pages/example/Tailwind.vue
+++ src/pages/example/Tailwind.vue
@@ -1,3 +1,13 @@
+<script setup>
+const toggleDarkMode = () => {
+  document.documentElement.classList.toggle('dark');
+};
+</script>
+
 <template>
-  <div>Tailwind</div>
+  <div class="flex h-screen items-center justify-center bg-red-100 sm:bg-yellow-100 md:bg-blue-100 lg:bg-green-100">
+    <div @click="toggleDarkMode" class="flex h-40 w-80 flex-col rounded-md bg-white p-4 shadow dark:bg-neutral-900">
+      <div class="h-10 w-10 rounded bg-blue-500"></div>
+    </div>
+  </div>
 </template>
