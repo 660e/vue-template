75c110bb2b7158b0dc08620cfbac381294831e6e
Min <solunar@vip.qq.com>
2026-05-08 21:55:18 +0800

    Update

--- src/views/example/Tailwind.vue
+++ src/views/example/Tailwind.vue
@@ -1,17 +1,13 @@
 <script setup>
 import { Pencil } from '@lucide/vue';
-
-const toggleDarkMode = () => {
-  document.documentElement.classList.toggle('dark');
-};
 </script>
 
 <template>
-  <div @click="toggleDarkMode" class="bg-background flex h-screen items-center justify-center">
-    <div class="h-40 w-80 rounded-lg bg-white p-4 shadow">
+  <div class="bg-background text-foreground flex h-screen items-center justify-center">
+    <div class="bg-card w-80 rounded-lg p-4 shadow">
       <div>
         <div class="bg-primary flex h-10 w-10 items-center justify-center rounded-md">
-          <Pencil :size="20" class="text-white" />
+          <Pencil :size="20" class="text-background" />
         </div>
       </div>
     </div>
