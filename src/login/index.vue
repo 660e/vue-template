53b6ec0acb29e196862b8fc2ce53a3cb83c45dbd
Min <solunar@vip.qq.com>
2026-05-11 20:25:10 +0800

    Update

--- src/login/index.vue
+++ src/login/index.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { User } from '@lucide/vue';
+import { User, Lock } from '@lucide/vue';
 
 const formData = reactive({
   username: '',
@@ -9,7 +9,7 @@ const formData = reactive({
 
 <template>
   <div class="bg-background flex h-screen items-center justify-center">
-    <div class="bg-card w-100 rounded-lg p-8 shadow">
+    <form class="bg-card text-card-foreground w-100 space-y-4 rounded-lg p-8 shadow">
       <div class="relative">
         <User :size="20" class="text-muted-foreground absolute top-1/2 left-3 -translate-y-1/2" />
         <input
@@ -20,6 +20,19 @@ const formData = reactive({
           type="text"
         />
       </div>
-    </div>
+      <div class="relative">
+        <Lock :size="20" class="text-muted-foreground absolute top-1/2 left-3 -translate-y-1/2" />
+        <input
+          v-model="formData.password"
+          autocomplete="current-password"
+          class="border-border focus:border-primary focus:ring-primary/30 h-10 w-full rounded-md border pr-3 pl-10 duration-200 outline-none focus:ring-3"
+          name="password"
+          type="password"
+        />
+      </div>
+      <button class="bg-primary hover:bg-primary-hover text-primary-foreground w-full cursor-pointer rounded-md py-2 duration-200" type="submit">
+        Sign in
+      </button>
+    </form>
   </div>
 </template>
