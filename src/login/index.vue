90661e86566f055a191c475398a722628f21576b
Min <solunar@vip.qq.com>
2026-05-13 21:28:25 +0800

    Update

--- src/login/index.vue
+++ src/login/index.vue
@@ -5,11 +5,15 @@ const formData = reactive({
   username: '',
   password: '',
 });
+
+function handleSubmit() {
+  console.log(formData);
+}
 </script>
 
 <template>
   <div class="bg-background flex h-screen items-center justify-center">
-    <form class="bg-card text-card-foreground w-100 space-y-4 rounded-lg p-8 shadow">
+    <form @submit.prevent="handleSubmit" class="bg-card text-card-foreground w-100 space-y-4 rounded-lg p-8 shadow">
       <div class="relative">
         <User :size="20" class="text-muted-foreground absolute top-1/2 left-3 -translate-y-1/2" />
         <input
@@ -19,7 +23,6 @@ const formData = reactive({
           name="username"
           type="text"
           autofocus
-          required
         />
       </div>
       <div class="relative">
@@ -30,7 +33,6 @@ const formData = reactive({
           class="border-border focus:border-primary focus:ring-primary/30 h-10 w-full rounded-md border pr-3 pl-10 duration-200 outline-none focus:ring-3"
           name="password"
           type="password"
-          required
         />
       </div>
       <button class="bg-primary hover:bg-primary-hover text-primary-foreground w-full cursor-pointer rounded-md py-2 duration-200" type="submit">
