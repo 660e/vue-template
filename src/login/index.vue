5d036d94ac3fc326a5570c0ef09525b829aad7b5
Min <solunar@vip.qq.com>
2026-06-01 20:52:38 +0800

    Update

--- src/login/index.vue
+++ src/login/index.vue
@@ -1,16 +1,28 @@
 <script setup>
-import { User, Lock } from '@lucide/vue';
+import { User, Lock, ScanBarcode } from '@lucide/vue';
 import { z } from 'zod';
 
 const formSchema = z.object({
   username: z.string().min(1, 'Username is required'),
   password: z.string().min(1, 'Password is required'),
+  code: z.string().min(1, 'Code is required'),
 });
 const formData = reactive({
   username: '',
   password: '',
+  code: '',
 });
 
+const codeImage = ref('');
+function refreshCode() {
+  codeImage.value = `https://picsum.photos/200/200?${Date.now()}`;
+}
+
+onMounted(() => {
+  refreshCode();
+});
+
+const errorMessage = ref('');
 function handleSubmit() {
   const result = formSchema.safeParse(formData);
 
@@ -21,40 +33,57 @@ function handleSubmit() {
     console.log(result.error.issues);
     console.log(z.treeifyError(result.error));
     console.log(result.error.message);
+
+    errorMessage.value = result.error.issues[0].message;
   }
 }
 </script>
 
 <template>
-  <div class="bg-background flex h-screen items-center justify-center">
-    <form @submit.prevent="handleSubmit" class="bg-card text-card-foreground w-100 space-y-4 rounded-lg p-8 shadow">
-      <div class="relative">
-        <User :size="20" class="text-muted-foreground absolute top-1/2 left-3 -translate-y-1/2" />
+  <div class="flex h-screen items-center justify-center">
+    <form @submit.prevent="handleSubmit" class="bg-card w-100 space-y-4 rounded-lg p-8 shadow">
+      <div class="text-muted-foreground relative">
+        <User :size="20" class="absolute top-1/2 left-3 -translate-y-1/2" />
         <input
           v-model="formData.username"
           autocomplete="username"
-          class="border-border focus:border-primary focus:ring-primary/30 h-10 w-full rounded-md border pr-3 pl-10 duration-200 outline-none focus:ring-3"
+          class="border-border focus:border-brand focus:ring-brand/30 h-10 w-full rounded-md border pr-3 pl-10 duration-200 outline-none placeholder:text-sm focus:ring-3"
           name="username"
+          placeholder="Username"
           type="text"
           autofocus
         />
       </div>
-      <div class="relative">
-        <Lock :size="20" class="text-muted-foreground absolute top-1/2 left-3 -translate-y-1/2" />
+      <div class="text-muted-foreground relative">
+        <Lock :size="20" class="absolute top-1/2 left-3 -translate-y-1/2" />
         <input
           v-model="formData.password"
           autocomplete="current-password"
-          class="border-border focus:border-primary focus:ring-primary/30 h-10 w-full rounded-md border pr-3 pl-10 duration-200 outline-none focus:ring-3"
+          class="border-border focus:border-brand focus:ring-brand/30 h-10 w-full rounded-md border pr-3 pl-10 duration-200 outline-none placeholder:text-sm focus:ring-3"
           name="password"
+          placeholder="Password"
           type="password"
         />
       </div>
-      <button class="bg-primary hover:bg-primary-hover text-primary-foreground w-full cursor-pointer rounded-md py-2 duration-200" type="submit">
+      <div class="text-muted-foreground relative flex gap-4">
+        <ScanBarcode :size="20" class="absolute top-1/2 left-3 -translate-y-1/2" />
+        <input
+          v-model="formData.code"
+          autocomplete="one-time-code"
+          class="border-border focus:border-brand focus:ring-brand/30 h-10 w-full rounded-md border pr-3 pl-10 duration-200 outline-none placeholder:text-sm focus:ring-3"
+          name="code"
+          placeholder="Code"
+          type="text"
+        />
+        <img :src="codeImage" @click="refreshCode" class="border-border h-10 w-30 shrink-0 cursor-pointer rounded-md border" />
+      </div>
+      <button class="bg-brand hover:bg-brand-hover text-brand-foreground w-full cursor-pointer rounded-md py-2 duration-200" type="submit">
         Sign in
       </button>
+      <div v-if="errorMessage">{{ errorMessage }}</div>
       <div class="flex justify-between">
-        <button class="text-primary cursor-pointer text-sm leading-none hover:underline" type="button">Forgot password?</button>
-        <button class="text-primary cursor-pointer text-sm leading-none hover:underline" type="button">Create an account</button>
+        <button class="text-brand cursor-pointer text-sm leading-none hover:underline" type="button">Forgot password?</button>
+        <button class="text-brand cursor-pointer text-sm leading-none hover:underline" type="button">Create an account</button>
       </div>
     </form>
   </div>
